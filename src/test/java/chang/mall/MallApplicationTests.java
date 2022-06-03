b9a45bb6b9f09e8bc617d70d9d3a56feef5264e5
Chang Ge <chang.techroad@gmail.com>
2022-06-02 21:57:59 -0400

    initial commit

---
+++ src/test/java/chang/mall/MallApplicationTests.java
@@ -0,0 +1,13 @@
+package chang.mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
